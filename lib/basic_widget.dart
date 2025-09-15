dce878b9e59d85513d150d93cad49012879b220c
fikri210604 <2317051061@student.unila.ac.id>
2025-09-15 01:19:14 +0000

    Pertemuan 3: Basic Widget

---
+++ lib/basic_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BasicWidget extends StatelessWidget {
+  const BasicWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Logo Resmi Kami')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png', width: 200, height: 200),
+            const SizedBox(height: 50),
+            ElevatedButton(onPressed: () {}, child: const Text('Pencet ini')),
+          ],
+        ),
+      ),
+    );
+  }
+}
