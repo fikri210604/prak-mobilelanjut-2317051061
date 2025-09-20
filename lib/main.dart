cc9d2e1776e27ed609b5a0ca2e735eb2542b322f
fikri210604 <2317051061@student.unila.ac.id>
2025-09-20 07:07:27 +0000

    Pertemuan 4 : Widget Lanjutan

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:myapp/basic_widget.dart';
 import 'package:myapp/column_widget.dart';
 import 'package:myapp/row_widget.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Praktikkum Mobile Lanjut',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const BasicWidget(),
-
-    );
   }
-}
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+      @override
+        Widget build(BuildContext context) {
+            return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                        title: 'JustDuit',
+                              theme: ThemeData(
+                                      scaffoldBackgroundColor: const Color(0xFFF3F6F8),
+                                              useMaterial3: true,
+                                                      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88FF)),
+                                                              inputDecorationTheme: InputDecorationTheme(
+                                                                        isDense: true,
+                                                                                  fillColor: Colors.white,
+                                                                                            filled: true,
+                                                                                                      contentPadding: const EdgeInsets.symmetric(
+                                                                                                                  horizontal: 16,
+                                                                                                                              vertical: 14,
+                                                                                                                                        ),
+                                                                                                                                                  border: OutlineInputBorder(
+                                                                                                                                                              borderRadius: BorderRadius.circular(12),
+                                                                                                                                                                          borderSide: BorderSide.none,
+                                                                                                                                                                                    ),
+                                                                                                                                                                                              enabledBorder: OutlineInputBorder(
+                                                                                                                                                                                                          borderRadius: BorderRadius.circular(12),
+                                                                                                                                                                                                                      borderSide: const BorderSide(color: Color(0xFFE0E0E0)),
+                                                                                                                                                                                                                                ),
+                                                                                                                                                                                                                                          focusedBorder: OutlineInputBorder(
+                                                                                                                                                                                                                                                      borderRadius: BorderRadius.circular(12),
+                                                                                                                                                                                                                                                                  borderSide: const BorderSide(color: Color(0xFF1E88FF)),
+                                                                                                                                                                                                                                                                            ),
+                                                                                                                                                                                                                                                                                    ),
+                                                                                                                                                                                                                                                                                          ),
+                                                                                                                                                                                                                                                                                                initialRoute: LoginScreen.route,
+                                                                                                                                                                                                                                                                                                      routes: {
+                                                                                                                                                                                                                                                                                                              '/': (_) => const LoginScreen(),
+                                                                                                                                                                                                                                                                                                                      '/signin': (_) => const LoginScreen(),
+                                                                                                                                                                                                                                                                                                                              '/signup': (_) => const SignupScreen(),
+                                                                                                                                                                                                                                                                                                                                    },
+                                                                                                                                                                                                                                                                                                                                        );
+                                                                                                                                                                                                                                                                                                                                          }
+                                                                                                                                                                                                                                                                                                                                          }
+                                                                                                                                                                                                                                                                                                                                          
